b134291138e4233b67e6594040947d3a1ef334dd
Afzal <afzalashraf814@gmail.com>
2024-08-16 13:55:50 +0500

    replace MessageBox with Notification

--- battery.py
+++ battery.py
@@ -1,37 +1,50 @@
 import ctypes
 from ctypes import wintypes
 import time
-import winsound
 import threading
+from plyer import notification
+
+
+def send_notification(message):
+    notification.notify(
+        title="Battery Alert",
+        message=message,
+        app_name="Battery Alert",
+        timeout=3,
+    )
+
 
 class SYSTEM_POWER_STATUS(ctypes.Structure):
     _fields_ = [
-        ('ACLineStatus', wintypes.BYTE),
-        ('BatteryFlag', wintypes.BYTE),
-        ('BatteryLifePercent', wintypes.BYTE),
-        ('Reserved1', wintypes.BYTE),
-        ('BatteryLifeTime', wintypes.DWORD),
-        ('BatteryFullLifeTime', wintypes.DWORD),
+        ("ACLineStatus", wintypes.BYTE),
+        ("BatteryFlag", wintypes.BYTE),
+        ("BatteryLifePercent", wintypes.BYTE),
+        ("Reserved1", wintypes.BYTE),
+        ("BatteryLifeTime", wintypes.DWORD),
+        ("BatteryFullLifeTime", wintypes.DWORD),
     ]
 
-def play_sound():
-    winsound.Beep(666, 4000)
-        
+
 while True:
     SYSTEM_POWER_STATUS_P = ctypes.POINTER(SYSTEM_POWER_STATUS)
-
     GetSystemPowerStatus = ctypes.windll.kernel32.GetSystemPowerStatus
     GetSystemPowerStatus.argtypes = [SYSTEM_POWER_STATUS_P]
     GetSystemPowerStatus.restype = wintypes.BOOL
-
     status = SYSTEM_POWER_STATUS()
-    MessageBox = ctypes.windll.user32.MessageBoxExW
-    if not GetSystemPowerStatus(ctypes.pointer(status)):
-        raise ctypes.WinError()
-    if status.BatteryLifePercent < 30 and status.ACLineStatus == 0:
-        threading.Thread(target=play_sound).start()
-        MessageBox(None, 'Please Connect Charger!\nBattery is less than 30%', 'Battery Alert - YDRGZM', 0x40000)
-    elif status.BatteryLifePercent > 90 and status.ACLineStatus == 1:
-        threading.Thread(target=play_sound).start()
-        MessageBox(None, 'Please Disconnect Charger!\nBattery is more than 90%', 'Battery Alert - YDRGZM', 0x40000)
-    time.sleep(30)
+
+    if GetSystemPowerStatus(ctypes.pointer(status)):
+        print(f"Battery Life: {status.BatteryLifePercent}%")
+        if status.BatteryLifePercent < 25 and status.ACLineStatus == 0:
+            t = threading.Thread(
+                target=send_notification,
+                args=["Battery is less than 25%. Connect your charger."],
+            ).start()
+        elif status.BatteryLifePercent > 80 and status.ACLineStatus == 1:
+            t = threading.Thread(
+                target=send_notification,
+                args=["Battery is more than 80%. Disconnect your charger."],
+            ).start()
+    else:
+        print("Failed to get system power status")
+
+    time.sleep(60)
